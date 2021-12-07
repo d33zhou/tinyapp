0419095d85574b6f5f4a014263e0bc88e93d4441
d33zhou <d33zhou@outlook.com>
2021-12-07 23:47:14 +0000

    add logic to state user logged in when userID cookies present, on each page

--- express_server.js
+++ express_server.js
@@ -4,8 +4,14 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+// MIDDLEWARE ---------------------------------------
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// LOCAL DATABASE -----------------------------------
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,16 +33,26 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies.username,
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    username: req.cookies.username,
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -45,7 +61,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-// POST METHODS --------------------------------------
+// POST METHODS -------------------------------------
 
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
@@ -69,11 +85,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${generatedString}`);
 });
 
-// SERVER --------------------------------------
+// SERVER -------------------------------------------
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// FUNCTIONS ----------------------------------------
 
 function generateRandomString(letters = 6) {
   const charPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";

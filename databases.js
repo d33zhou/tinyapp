f2da6bc0ab014ef916a0a27e68e953042894c56c
d33zhou <d33zhou@outlook.com>
2021-12-09 21:57:20 +0000

    database data changes for testing purposes

--- databases.js
+++ databases.js
@@ -5,37 +5,37 @@ const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
     userID: "aaBB12",
-    visits: 7
+    visits: 1
   },
   "9sm5xK": {
     longURL: "http://www.google.ca",
     userID: "aaBB12",
-    visits: 32
+    visits: 4
   },
   "e7HeP0": {
     longURL: "http://www.outlook.com",
     userID: "aaBB12",
-    visits: 3
+    visits: 0
   },
   "ySC21a": {
     longURL: "http://www.reddit.com",
     userID: "aaBB12",
-    visits: 67
+    visits: 4
   },
   "pP0a9W": {
     longURL: "http://www.youtube.com",
     userID: "aaBB12",
-    visits: 23
+    visits: 3
   },
   "nZxx6A": {
     longURL: "http://www.amazon.ca",
     userID: "CCdd34",
-    visits: 14
+    visits: 2
   },
   "267Ad1": {
     longURL: "http://www.google.ca",
     userID: "CCdd34",
-    visits: 8
+    visits: 0
   },
 };
 
@@ -44,12 +44,12 @@ const users = {
   aaBB12: {
     id: "aaBB12",
     email: "user@example.com",
-    password: "test1"
+    password: "$2a$10$az8pGYtjheTDV4dmBY3XFuE4MRj5P3TdxSe7C7QUFYF.LCk2ZNZta" // test
   },
   CCdd34: {
     id: "CCdd34",
     email: "test@example.com",
-    password: "test2"
+    password: "$2a$10$sxbZ4ZNB5iyX1eYExmVmZOstSu2sPmSDhaINQ/OhBxQR9H8OKdh7a" // test
   },
 };
 
